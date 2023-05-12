6de20002b5332f01616544bedc85e262a0d37f6e
iDannyGR <86504340+iDannyGR@users.noreply.github.com>
2023-05-12 11:53:39 -0600

    update

--- src/components/ClassState.tsx
+++ src/components/ClassState.tsx
@@ -9,19 +9,24 @@ class ClasState extends React.Component {
       <div className="border w-full h-[50vh] flex flex-col items-center justify-center space-y-6">
         <h2 className="text-5xl">Eliminar {name}</h2>
         <p className="text-xl">escribe tu codigo de seguridad</p>
-        <div>
-          <input
-            type="text"
-            placeholder="secure code"
-            className="border border-gray-400 shadow py-2 px-10 rounded-2xl mr-7"
-          />
-          <button 
-          className="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow"
-          onClick={() => this.setState({error:!error})}
-          >
-            comprobar
-          </button>
-        </div>
+        <form
+          onSubmit={e => e.preventDefault() }
+        >
+          {error && <p className="text-red-500">wrong code</p>}
+          <div>
+            <input
+              type="text"
+              placeholder="secure code"
+              className="border border-gray-400 shadow py-2 px-10 rounded-2xl mr-7"
+            />
+            <button
+              className="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow"
+              onClick={() => this.setState({ error: !error })}
+            >
+              comprobar
+            </button>
+          </div>
+        </form>
       </div>
     );
   }
--- src/components/UseState.tsx
+++ src/components/UseState.tsx
@@ -8,24 +8,46 @@ type props = {
 const UseState:React.FC<props> = ({name}):React.ReactElement => {
     
   const [error, setError] = React.useState<boolean>(false);
+  const [loading, setLoading] = React.useState<boolean>(false)
+  
+  React.useEffect(() => {
+
+    console.log('start effect')
+
+      if(!!loading){ 
+        setTimeout(()=>{
+        console.log('start validation')
+        
+        setLoading(false)
+        
+        console.log('end validation')
+      },5000)}
+    
+      console.log('end effect')
+
+  }, [loading])
   
   return (
     <div className="border w-full h-[50vh] flex flex-col items-center justify-center space-y-6">
       <h2 className="text-5xl">Eliminar {name}</h2>
       <p className="text-xl">escribe tu codigo de seguridad</p>
-      {error && <p className="text-red-500">wrong code</p>}
-      <div>
-        <input 
-        type="text" 
-        placeholder="secure code" 
-        className="border border-gray-400 shadow py-2 px-10 rounded-2xl mr-7" />
-        <button
-          onClick={() => setError(!error)}
-          className="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow"
-        >
-          comprobar
-        </button>
-      </div>
+      <form onSubmit={(e) => e.preventDefault()}>
+        {error && <p className="text-red-500">wrong code</p>}
+        {loading && <p className="text-red-500">cargando</p>}
+        <div>
+          <input
+            type="text"
+            placeholder="secure code"
+            className="border border-gray-400 shadow py-2 px-10 rounded-2xl mr-7"
+          />
+          <button
+            onClick={() => setLoading(true)}
+            className="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow"
+          >
+            comprobar
+          </button>
+        </div>
+      </form>
     </div>
   );
 };
